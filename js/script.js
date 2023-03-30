e0fe203b4571abda000f747d4b3f07a292804572
Awele1111 <awelelan@Aweles-MacBook-Air.local>
2023-03-30 03:51:02 -0500

    edited js file

--- js/script.js
+++ js/script.js
@@ -23,9 +23,11 @@ windows.onscroll = () => {
         })
         }
     })
+
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100)
+
 }
 
-let header = document.querySelector('header');
-
-header.classList.toggle('sticky', window.scrollY > 100)
 
